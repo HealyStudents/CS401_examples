2912a818f3b57678149308d2e73a3a0d4288aced
Pat Healy <pwh5@pitt.edu>
2023-01-17 12:24:41 -0500

    Updated after class

--- Week01/src/week01/FortuneTeller.java
+++ Week01/src/week01/FortuneTeller.java
@@ -15,25 +15,40 @@ public class FortuneTeller
 
 	public static void main(String[] args) 
 	{
+		//Welcome the user
+		JOptionPane.showMessageDialog(null, "Welcome! Fortunes! Ahhhhhh!!!!!");
+		
+		// TODO ask them their name and age
+		String name = JOptionPane.showInputDialog("What's your name?");
+		int age = Integer.parseInt(JOptionPane.showInputDialog("What's your age?")); //Takes in the user's age and converts it to an integer
+		
 		// TODO Instantiate a magic number (Integer)
+		String magicString = JOptionPane.showInputDialog(null, "Please input a magic integer.");
+		int magicInt = Integer.parseInt(magicString);
 		
 		// TODO Instantiate a magic divisor (Floating Point Number)
-		
-		// TODO Use a JOptionPane to welcome the user
-		// 			and ask them their name and age
+		magicString = JOptionPane.showInputDialog(null, "Please input a magic float.");
+		float magicFloat = Float.parseFloat(magicString);
 		
 		// TODO Calculate a number of days using this formula:
 		// 			Calculate integer x:
 		//				divide their age by the magic number
 		//				add this ^ to the age modulo the magic number
+		int x = age / magicInt;
+		x = x + (age % magicInt);
 		// 			Calculate integer y:
 		//				take the length of their name
 		//				divide it by the magic divisor
 		//				Round this quotient to the nearest whole number
+		int y = Math.round(name.length() / magicFloat);
+		
+		
+		// days is going to be x + y, modulo magic number
+		int days = (x + y) % magicInt;
 		
 		// Print this message to the user
-		//System.out.println("Thank you, " + name);
-		//System.out.println("The shadows from above said that you will be given an important oportunity in the future, in exactly " + days + " days from now. \nDo not miss it!");
+		JOptionPane.showMessageDialog(null, "Thank you, " + name);
+		JOptionPane.showMessageDialog(null, "The shadows from above said that you will be given an important oportunity in the future, in exactly " + days + " days from now. \nDo not miss it!");
 
 	}
 
--- Week01/src/week01/Variables.java
+++ Week01/src/week01/Variables.java
@@ -13,23 +13,29 @@ public class Variables
 	public static void main(String[] args) 
 	{
 		//8. Is this a legal declaration/initialization? Why?
-		// char myFavoriteCharacter = "a";
+		//char myFavoriteCharacter = "a";
 
 		//9. How would you make a string of one character, 
 		// 		taken from the start of the string defined below?
-		// String city = "Pittsburgh";
+		String city = "Pittsburgh";
+		char firstChar = city.charAt(0);
 
 		//10. How would you calculate the length of the following string:
-		// "Java Program"
+		String text = "Java Program";
+		int len = text.length();
 		
 		// Swing IO Example
-		//String input = JOptionPane.showInputDialog("Write your name");
-		//JOptionPane.showMessageDialog(null,"Hi " + input);
+		JOptionPane.showMessageDialog(null, "Hi there!");
+		String input = JOptionPane.showInputDialog("Write your name");
+		System.out.println(input);
+		JOptionPane.showMessageDialog(null,"Hi " + input);
 		
 		// 11. Given the following code, 
-		// 		how would you convert the value of myString to an integer?
-		//String myString = "745";
-		//int myInteger = ?;
+		// 		how would you convert the 
+		// value of myString to an integer?
+		String myString = "745";
+		int myInteger = Integer.parseInt(myString);
+		System.out.println(myInteger + 3);
 	}
 
 }
