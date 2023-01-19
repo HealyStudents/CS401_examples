45413569256f64c5ac58e88a546fcb053191c1cf
Pat Healy <pwh5@pitt.edu>
2023-01-19 11:09:43 -0500

    Adding templates of two files for 1/19/23

---
+++ Week01/src/week01/IfStatements.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package week01;
+
+/**
+ * @author Pat Healy
+ *
+ */
+public class IfStatements 
+{
+
+	public static void main(String[] args) 
+	{
+		// Exercise 1
+		// Assume a variable x contains a double value. 
+		// Write an if-else statement that multiplies it by itself 
+		// if the value is greater than 1. Otherwise it should divide it by 2.
+		
+		//Exercise 1.5
+		//Using an if statement and the modulus (%) operator write a program that 
+		// asks users to enter a number and determines whether 
+		// the number entered is even or odd.
+		
+		//Ask user an input
+		//Convert input to number
+		//if reminder of number / 2 is 0, print is even, otherwise print odd
+		
+		
+		
+		// Exercise 2
+		//What's wrong with the following block of code?
+		/*if(accountBalance = 0){
+		        System.out.println(“No money in your account!”);
+		}*/
+		
+		//Exercise 3
+		// Supply a condition in this if statement to test whether the user entered a Y:
+		/*String input = JOptionPane.showInputDialog("Enter Y to quit.");
+		if (. . .){
+			System.out.println("Goodbye.");
+		}*/
+		
+		//Exercise 4
+		// of the following conditions are true, provided a = 3 and b = 4?
+		/*a + 1 <= b
+				a + 1 >= b
+				a + 1 != b*/
+		
+		// Exercise 5
+		// What is the value of !!frozen?
+		//when frozen is true, !!frozen is  ____
+		//when frozen is false, !!frozen is ____
+
+
+		//Exercise 6
+		//Suppose x and y are two integers. 
+		//How do you test whether both of them are zero?
+		//How do you test whether at least one of them is zero?
+		//How do you test whether exactly one of them is zero?
+		
+		
+
+	}
+	
+}
